8e19a45e23d2dbe06e7b73955dcb7bdc19d29446
harry101491 <hp1014@nyu.edu>
2022-01-02 12:49:36 -0800

    task(adding changes for adding two linked list numbers)

--- AddingTwoNumbers.java
+++ AddingTwoNumbers.java
@@ -38,8 +38,18 @@ public class AddingTwoNumbers {
         }
         Node newNum = null;
         Integer carry = 0;
-        while (num1 != null && num2 != null) {
-            Integer sum = num1.value + num2.value + carry;
+        while (num1 != null || num2 != null) {
+            Integer sum = 0;
+            if (num1 == null) {
+                sum = num2.value + carry;
+            } else if (num2 == null) {
+                sum = num1.value + carry;
+            } else {
+                sum = num1.value + num2.value + carry;
+            }
+            if (carry != 0) {
+                carry = 0;
+            }
             Node newNode = null;
             if (sum >= 10) {
                 newNode = new Node(sum % 10);
@@ -52,8 +62,17 @@ public class AddingTwoNumbers {
             } else {
                 newNum = Node.addNewNode(newNum, newNode);
             }
-            num1 = num1.next;
-            num2 = num2.next;
+            if (num1 == null && num2 != null) {
+                num2 = num2.next;
+            } else if (num2 == null && num1 != null) {
+                num1 = num1.next;
+            } else {
+                num1 = num1.next;
+                num2 = num2.next;
+            }
+        }
+        if (carry != 0) {
+            newNum = Node.addNewNode(newNum, new Node(carry));
         }
         return newNum;
     }
@@ -61,12 +80,30 @@ public class AddingTwoNumbers {
     public static void main(String args[]) {
         Node num1 = null;
         Node num2 = null;
-        num1 = Node.addNewNode(num1, new Node(2));
-        num1 = Node.addNewNode(num1, new Node(4));
-        num1 = Node.addNewNode(num1, new Node(3));
-        num2 = Node.addNewNode(num2, new Node(5));
-        num2 = Node.addNewNode(num2, new Node(6));
-        num2 = Node.addNewNode(num2, new Node(4));
+
+        num1 = Node.addNewNode(num1, new Node(9));
+        num1 = Node.addNewNode(num1, new Node(9));
+        num1 = Node.addNewNode(num1, new Node(9));
+        num1 = Node.addNewNode(num1, new Node(9));
+        num1 = Node.addNewNode(num1, new Node(9));
+        num1 = Node.addNewNode(num1, new Node(9));
+        num1 = Node.addNewNode(num1, new Node(9));
+
+        num2 = Node.addNewNode(num2, new Node(9));
+        num2 = Node.addNewNode(num2, new Node(9));
+        num2 = Node.addNewNode(num2, new Node(9));
+        num2 = Node.addNewNode(num2, new Node(9));
+
+        // num1 = Node.addNewNode(num1, new Node(2));
+        // num1 = Node.addNewNode(num1, new Node(4));
+        // num1 = Node.addNewNode(num1, new Node(3));
+
+        // num2 = Node.addNewNode(num2, new Node(5));
+        // num2 = Node.addNewNode(num2, new Node(6));
+        // num2 = Node.addNewNode(num2, new Node(4));
+
+        // num1 = Node.addNewNode(num1, new Node(0));
+        // num2 = Node.addNewNode(num2, new Node(0));
 
         // Node.printNode(num1);
         // Node.printNode(num2);
